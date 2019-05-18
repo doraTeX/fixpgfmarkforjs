0ec5b27e8534aa7205bdbc3e724baa1c68c5f5bc
doraTeX <taylorkgb@gmail.com>
2019-05-19 00:37:16 +0900

    Adopt bxpgfcurpage.sty

--- testAll.sh
+++ testAll.sh
@@ -3,7 +3,7 @@
 (cd tests
 
 for FILE in {gentombow_nopdfbox,gentombow_pdfbox,notombow,tombow,geometry}_{8pt,14pt}.tex ; do
-  ptex2pdf -u -l ${FILE}
+  uplatex ${FILE}
   ptex2pdf -u -l ${FILE}
 done
 
